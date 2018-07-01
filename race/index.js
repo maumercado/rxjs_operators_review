0850a1e186c33d316f8887b7e203a83569e1d5d3
Mauricio Mercado <mau@maumercado.com>
2018-06-30 22:24:47 -0700

    Removed console.logs from map in race

--- race/index.js
+++ race/index.js
@@ -1,7 +1,7 @@
 import { race, timer } from "rxjs";
 import { map } from "rxjs/operators";
 
-const apiA$ = timer(1500, 100).pipe(map(n => console.log(`API A ${n}`)));
-const apiB$ = timer(2000, 100).pipe(map(n => console.log(`API B ${n}`)));
+const apiA$ = timer(1500, 100).pipe(map(n => `API A ${n}`));
+const apiB$ = timer(2000, 100).pipe(map(n => `API B ${n}`));
 
 race(apiA$, apiB$).subscribe(n => console.log(n));
